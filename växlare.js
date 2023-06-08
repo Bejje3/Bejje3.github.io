5a1f26c9f88a7dbc69c30c3f3b099b5a2e128915
Benjamin Andersson Veizades <bean0905@storsjogymnasiet.se>
2023-06-08 11:05:41 +0200

    min sida

--- växlare.js
+++ växlare.js
@@ -1,56 +1,171 @@
 class Pengar {
-constructor(antal, pengsort) {
-  this.antal = antal 
-  this.pengsort = pengsort 
-   }
-} 
+    constructor(antal, pengsort) {
+        this.antal = antal
+        this.pengsort = pengsort
+    }
 
-/**
- * exChange
- * in: belop som ska växlas, valör på sedlen 
- * out: antal sedlar eller mynt 
- */ 
-function exChange(belopp, sedel){
-    return Math.floor(belopp / sedel) 
 }
-    
-/**
- * getExchangeArray()
- * in inbetalt belopp 
- * out: array innehåller antal mynt i varje valör 
- */ 
-function getExchangeArray(inbetalning, priset){
 
-    let antal_mynt = 0  
-    let pengar_tillbaka = 0 
+const input_pris = document.getElementById("pris")
+const input_inbet = document.getElementById("inbet")
+
+const svardiv = document.getElementById("svar_div")
+
+const kalkyleraknapp = document.getElementById("kalk_knapp")
+kalkyleraknapp.addEventListener("click", kalkyleraClick)
+/* exChange 
+ * in: belopp som ska växlas
+ * out: antal sedlar eller mynt 
+ */
+
+
+function exChange(belopp, sedel) {
+    return Math.floor(belopp / sedel)
+}
+
+/
+ * getexchangeArray()
+ * in inbetalt belopp
+ * out: array innehåller antal mynt i varje valör
+ */
+function getexchangeArray(inbetalning, priset) {
+
+    let antal_mynt = 0
+    let pengar_tillbaka = 0
 
     sedlar_mynt_array = []
-    
-    //500---------------------------------
-    pengar_tillbaka = inbetalning - priset 
-    antal_mynt = exChange(pengar_tillbaka, 500) 
-    console.log(`antal= ${antal_mynt}` ) 
-        
-        
+
+    //500.............................................
+    pengar_tillbaka = inbetalning - priset
+    antal_mynt = exChange(pengar_tillbaka, 500)
+    pengar_tillbaka = pengar_tillbaka % 500
+
+    let t_pengar = new Pengar(0, 500)
+
+
+    if (antal_mynt > 0){
+        t_pengar = antal_mynt
+    }
+
+    sedlar_mynt_array.push(t_pengar)
+
+    console.log(t_pengar= ${t_pengar})
+
+    //100.............................................
+    antal_mynt = exChange(pengar_tillbaka, 100)
+    pengar_tillbaka = pengar_tillbaka % 100
+
+    t_pengar = new Pengar(0, 100)
+
+
+    if (antal_mynt > 0){
+        t_pengar = antal_mynt
+    }
+
+    sedlar_mynt_array.push(t_pengar)
+
+    console.log(t_pengar= ${t_pengar})
+
+
+ //50.............................................
+ antal_mynt = exChange(pengar_tillbaka, 50)
+ pengar_tillbaka = pengar_tillbaka % 50
+
+ t_pengar = new Pengar(0, 50)
+ 
+
+ if (antal_mynt > 0){
+     t_pengar = antal_mynt
+ }
+
+ sedlar_mynt_array.push(t_pengar)
+
+ console.log(t_pengar= ${t_pengar})
+
+  //20.............................................
+  antal_mynt = exChange(pengar_tillbaka, 20)
+  pengar_tillbaka = pengar_tillbaka % 20
+
+  t_pengar = new Pengar(0, 20)
+
+
+  if (antal_mynt > 0){
+      t_pengar = antal_mynt
+  }
+
+  sedlar_mynt_array.push(t_pengar)
+
+  console.log(t_pengar= ${t_pengar})
+
+   //10.............................................
+   antal_mynt = exChange(pengar_tillbaka, 10)
+   pengar_tillbaka = pengar_tillbaka % 10
+
+   t_pengar = new Pengar(0, 10)
+
+
+   if (antal_mynt > 0){
+       t_pengar = antal_mynt
+   }
+
+   sedlar_mynt_array.push(t_pengar)
+
+   console.log(t_pengar= ${t_pengar})
+
+ //5.............................................
+ antal_mynt = exChange(pengar_tillbaka, 5)
+ pengar_tillbaka = pengar_tillbaka % 5
+
+ t_pengar = new Pengar(0, 5)
+ 
+
+ if (antal_mynt > 0){
+     t_pengar = antal_mynt
+ }
+
+ sedlar_mynt_array.push(t_pengar)
+
+ console.log(t_pengar= ${t_pengar})
+
+ //2.............................................
+ antal_mynt = exChange(pengar_tillbaka, 2)
+ pengar_tillbaka = pengar_tillbaka % 2
+
+ t_pengar = new Pengar(0, 2)
+ 
+
+ if (antal_mynt > 0){
+     t_pengar = antal_mynt
+ }
+
+ sedlar_mynt_array.push(t_pengar)
+
+ console.log(t_pengar= ${t_pengar})
+
+ //1.............................................
+ antal_mynt = exChange(pengar_tillbaka, 1)
+ pengar_tillbaka = pengar_tillbaka % 1
+
+ t_pengar = new Pengar(0, 1)
+ 
+
+ if (antal_mynt > 0){
+     t_pengar = antal_mynt
+ }
+
+ sedlar_mynt_array.push(t_pengar)
+
+ console.log(t_pengar= ${t_pengar})
+
 }
 
 
+function kalkyleraClick(e) {
 
-const input_pris = document.getElementById("pris") 
-const input_inbet = document.getElementById("inbet") 
-const svardiv = document.getElementById("svar_div") 
+    let t_pris = parseInt(input_pris.value)
+    let t_inbet = parseInt(input_inbet.value)
 
-const kalkyleraknapp = document.getElementById("kalk_knapp") 
-kalkyleraknapp.addEventListener("click", kalkyleraClick) 
+    getexchangeArray(t_inbet, t_pris)
 
-function kalkyleraClick(e) { 
-
-
-   
-    let t_pris = parseInt(input_pris.value) 
-    let t_inbet = parseInt(input_inbet.value) 
-
-    getExchangeArray(t_inbet, t_pris) 
-console.log(`klick)`) 
-//console.log(`klick`) 
+    //console.log(klick)
 }
